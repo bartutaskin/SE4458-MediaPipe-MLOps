8bbcaf09d37d4157499fbc216ee9005669d418f1
Bartu Taskin <bartutaskin35@gmail.com>
2025-03-26 17:38:34 +0300

    Draw hand landmarks

--- HandTracking.py
+++ HandTracking.py
@@ -5,12 +5,29 @@ import time
 # Create a VideoCapture object to access the webcam
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
 
     if not success:
         print("Failed to capture image")
         break
+
+    # Convert the image to RGB format for MediaPipe
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    #print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
