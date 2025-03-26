1839f9c73a07ac5cf42f28a31d26d04219549650
Bartu Taskin <bartutaskin35@gmail.com>
2025-03-26 17:21:43 +0300

    Capture the webcam

--- HandTracking.py
+++ HandTracking.py
@@ -1,3 +1,17 @@
 import cv2
 import mediapipe as mp
 import time
+
+# Create a VideoCapture object to access the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    if not success:
+        print("Failed to capture image")
+        break
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
