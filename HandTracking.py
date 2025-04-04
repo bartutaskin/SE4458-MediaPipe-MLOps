0d1a107876a44522e5f664c876aa83194c391314
Bartu Taskin <bartutaskin35@gmail.com>
2025-04-04 16:07:40 +0300

    Add mlflow, update gitignore, and add requirements.txt

--- HandTracking.py
+++ HandTracking.py
@@ -1,6 +1,10 @@
 import cv2
 import mediapipe as mp
 import time
+import mlflow
+
+# Set the MLflow experiment name
+mlflow.set_experiment("HandTracking-FPS")
 
 # Create a VideoCapture object to access the webcam
 cap = cv2.VideoCapture(0)
@@ -15,39 +19,47 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
-while True:
-    success, img = cap.read()
-
-    if not success:
-        print("Failed to capture image")
-        break
-
-    # Convert the image to RGB format for MediaPipe
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    #print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c=img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                #print(id, cx,cy)
-                if id==4:
-                    cv2.circle(img, (cx,cy),15,(255,0,255), cv2.FILLED)
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+with mlflow.start_run():
+    # Log model parameters once at the beginning
+    mlflow.log_param("static_image_mode", False)
+    mlflow.log_param("max_num_hands", 2)
+    mlflow.log_param("min_detection_confidence", 0.5)
+    mlflow.log_param("min_tracking_confidence", 0.5)
     
-    cTime = time.time()
-    fps= 1/(cTime - pTime)
-    pTime = cTime
+    while True:
+        success, img = cap.read()
 
-    cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        if not success:
+            print("Failed to capture image")
+            break
 
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Convert the image to RGB format for MediaPipe
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        #print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    #print(id,lm)
+                    h, w, c=img.shape
+                    cx, cy = int(lm.x*w), int(lm.y*h)
+                    #print(id, cx,cy)
+                    if id==4:
+                        cv2.circle(img, (cx,cy),15,(255,0,255), cv2.FILLED)
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+        
+        cTime = time.time()
+        fps= 1/(cTime - pTime)
+        mlflow.log_metric("FPS", fps)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
